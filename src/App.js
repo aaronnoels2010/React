a7d7c84ca2f816b7d1d67fa49eb912973d976bff
aaronnoels <noels.aaron@gmail.com>
2020-02-22 22:44:05 +0100

    Expanded with search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchWord: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(body => this.setState({ monsters: body }));
+  }
+
+  search = event => {
+    this.setState({ searchWord: event.target.value });
+  };
+
+  render() {
+    const { monsters, searchWord } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().match(searchWord.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <SearchBox placeholder="Search monsters" handleSearch={this.search} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+
+  verwerk() {
+    const searchField = document.getElementsByTagName("input")[0].value;
+    this.setState({ searchWord: searchField });
+    console.log(`DATA: ${this.state.searchWord}`);
+  }
 }
 
 export default App;
